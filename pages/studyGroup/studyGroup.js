dd5557fab07978c6dbdaf7fe96f6909c7c400256
TallMessiWu <TallMessiWu@users.noreply.github.com>
2021-04-27 22:02:45 +0800

    图片保存功能的优化

--- pages/studyGroup/studyGroup.js
+++ pages/studyGroup/studyGroup.js
@@ -135,6 +135,60 @@ Page({
     }
   },
 
+  imageTap: function (e){
+    let url = e.currentTarget.dataset.url;
+    wx.previewImage({
+      urls: [url],
+      current: url
+    })
+  },
+
+  // saveImg(e){
+  //   let url = e.currentTarget.dataset.url;
+  //   //用户需要授权
+  //   wx.getSetting({
+  //    success: (res) => {
+  //     if (!res.authSetting['scope.writePhotosAlbum']) {
+  //      wx.authorize({
+  //       scope: 'scope.writePhotosAlbum',
+  //       success:()=> {
+  //        // 同意授权
+  //        this.saveImg1(url);
+  //       },
+  //       fail: (res) =>{
+  //        console.log(res);
+  //       }
+  //      })
+  //     }else{
+  //      // 已经授权了
+  //      this.saveImg1(url);
+  //     }
+  //    },
+  //    fail: (res) =>{
+  //     console.log(res);
+  //    }
+  //   })  
+  //  },
+  //  saveImg1(url){
+  //   wx.getImageInfo({
+  //    src: url,
+  //    success:(res)=> {
+  //     let path = res.path;
+  //     wx.saveImageToPhotosAlbum({
+  //      filePath:path,
+  //      success:(res)=> { 
+  //       console.log(res);
+  //      },
+  //      fail:(res)=>{
+  //       console.log(res);
+  //      }
+  //     })
+  //    },
+  //    fail:(res)=> {
+  //     console.log(res);
+  //    }
+  //   })
+  //  },
 
   /**
    * Lifecycle function--Called when page load
