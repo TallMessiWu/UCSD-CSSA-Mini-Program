5c722bc4c5de06598c6e417c8ba8da5a3d2eaeca
TallMessiWu <tallmessiwu@qq.com>
2021-11-02 15:32:48 +0800

    Update index.js

--- cloudfunctions/getArticles/index.js
+++ cloudfunctions/getArticles/index.js
@@ -10,7 +10,11 @@ const articles = cloud.database().collection("articles")
 // 云函数入口函数
 exports.main = async (event, context) => {
     const result = await articles.where({
-        collection: event.collectionName
+        // 用一个模糊查询来实现一个文章归属多个集合
+        collection: cloud.database().RegExp({
+            regexp: `.*${event.collectionName}.*`,
+            options: 'i'
+        })
     }).skip(event.start).orderBy("_createTime", "desc").limit(15).get().then((res) => {
         return res
     })
