56cbdca6cdd253d4ce3f281e0da3a6e59610d5c9
TallMessiWu <tallmessiwu@qq.com>
2021-12-02 16:37:27 +0800

    主页完成啦！！！

--- cloudfunctions/searchArticles/index.js
+++ cloudfunctions/searchArticles/index.js
@@ -4,6 +4,7 @@ const cloud = require('wx-server-sdk')
 cloud.init({
     env: cloud.DYNAMIC_CURRENT_ENV
 })
+
 LIMIT_SIZE = 15
 
 const articles = cloud.database().collection("articles")
@@ -11,13 +12,10 @@ const articles = cloud.database().collection("articles")
 // 云函数入口函数
 exports.main = async (event, context) => {
     const result = await articles.where({
-        // 用一个模糊查询来实现一个文章归属多个集合
-        collection: cloud.database().RegExp({
-            regexp: `.*${event.collectionName}.*`,
+        title: cloud.database().RegExp({
+            regexp: `.*${event.keyword}.*`,
             options: 'i'
         })
-    }).skip(event.start).orderBy("_createTime", "desc").limit(LIMIT_SIZE).get().then((res) => {
-        return res
-    })
+    }).skip(event.start).orderBy("_createTime", "desc").limit(LIMIT_SIZE).get()
     return result
 }
--- miniprogram/pages/discovery/discovery.js
+++ miniprogram/pages/discovery/discovery.js
@@ -1,6 +1,5 @@
 const classesId = "8f9ff639611a410d00aaa7ba5e2491d6"
 
-
 Page({
 
   /**
@@ -26,39 +25,45 @@ Page({
       }
     ],
     collections: [{
-        text: "精彩活动",
-        src: "/images/icons/discovery/xuexiao.png"
+        text: "CSSA简介",
+        src: "/images/cssa-logo.jpg"
       },
       {
-        text: "干货攻略",
-        src: "/images/icons/discovery/shuji.png"
-      },
-      {
-        text: "社团介绍",
-        src: "/images/icons/discovery/shetuanhuodong.png"
-      },
-      {
-        text: "校园帮助",
-        src: "/images/icons/discovery/diandeng.png"
-      },
-      {
-        text: "活动报名",
-        src: "/images/icons/discovery/baoming.png"
-      },
-      {
-        text: "帮助说明",
+        text: "TritonTalk",
         src: "/images/icons/discovery/baozhi.png"
       },
       {
-        text: "旅游活动",
-        src: "/images/icons/discovery/daba.png"
+        text: "干货攻略",
+        src: "/images/icons/discovery/ganhuogonglue.png"
       },
       {
-        text: "竞技比赛",
-        src: "/images/icons/discovery/fenlei-dota.png"
+        text: "联系我们",
+        src: "/images/icons/discovery/lianxiwomen.png"
+      },
+      {
+        text: "CSSA原创",
+        src: "/images/icons/discovery/diandeng.png"
+      },
+      {
+        text: "活动预告",
+        src: "/images/icons/discovery/huodongyugao.png"
+      },
+      {
+        text: "往期活动",
+        src: "/images/icons/discovery/wangqihuodong.png"
+      },
+      {
+        text: "新生指南",
+        src: "/images/icons/discovery/xinshengzhinan.png"
       }
     ],
-    headlines: []
+    headlines: [],
+    starredArticles: [],
+    isSearching: false,
+    searchResult: [],
+    keyword: "",
+    service: false,
+    searchTitle: "请输入你要搜索的课程名称...",
   },
 
   async setTabBar() {
@@ -70,9 +75,22 @@ Page({
   },
 
   onCollectionTap(event) {
+    if (this.data.service) {
+      wx.navigateTo({
+        url: '/pages/collection/collection?name=课程信息'
+      })
+      return
+    }
     let name = event.currentTarget.dataset.name
     wx.navigateTo({
       url: '/pages/collection/collection?name=' + name
+    })
+  },
+
+  onArticleTap(event) {
+    const content = event.currentTarget.dataset.article.content
+    wx.navigateTo({
+      url: `/pages/article/article?content=${content}&collectionName=精选推荐`,
     })
   },
 
@@ -80,6 +98,13 @@ Page({
     const content = event.currentTarget.dataset.article.articleInfo[0].content
     wx.navigateTo({
       url: `/pages/article/article?content=${content}&collectionName=头条`,
+    })
+  },
+
+  onSearchTap(event) {
+    const content = event.currentTarget.dataset.article.content
+    wx.navigateTo({
+      url: `/pages/article/article?content=${content}&collectionName=搜索`,
     })
   },
 
@@ -94,6 +119,70 @@ Page({
         headlines: res.result.list
       })
       wx.hideLoading()
+      wx.stopPullDownRefresh()
+    })
+  },
+
+  loadStarredArticles() {
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
+    wx.cloud.callFunction({
+      name: "getArticles",
+      data: {
+        collectionName: "精选推荐",
+        start: 0
+      }
+    }).then((res) => {
+      this.setData({
+        starredArticles: res.result.data.slice(0, 2)
+      })
+      wx.stopPullDownRefresh()
+      wx.hideLoading()
+    })
+  },
+
+  onSearch(event) {
+    const keyword = event.detail.value.trim()
+    if (keyword == "") {
+      this.onSearchCancel()
+      return
+    }
+    this.setData({
+      keyword,
+      isSearching: true
+    })
+    
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
+    if (this.data.service) {
+      this.setData({
+        searchResult: []
+      })
+      wx.hideLoading()
+      return
+    }
+    wx.cloud.callFunction({
+      name: "searchArticles",
+      data: {
+        keyword,
+        start: this.data.searchResult.length
+      }
+    }).then((res) => {
+      this.setData({
+        searchResult: res.result.data
+      })
+      wx.hideLoading()
+    })
+  },
+
+  onSearchCancel() {
+    this.setData({
+      isSearching: false,
+      searchResult: []
     })
   },
 
@@ -101,8 +190,52 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
     this.setTabBar()
-    this.loadHeadlines()
+    wx.cloud.database().collection("card_info").doc("cd045e756142fe8d0e6e827f32aeb3cb").get().then((res) => {
+      this.setData({
+        service: res.data.service
+      })
+      if (!this.data.service) {
+        this.setData({
+          searchTitle: "请输入你要搜索的文章标题..."
+        })
+        this.loadHeadlines()
+        this.loadStarredArticles()
+      } else {
+        this.setData({
+          headlines: [{
+            poster: "cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class1.jpg",
+            articleInfo: [{
+              collection: "CSSA简介 TritonTalk 干货攻略 联系我们 CSSA原创 活动预告 往期活动 新生指南",
+              content: ["cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class1.jpg"],
+              title: "课程信息1",
+              summary: "课程简介",
+              thumbnail: "cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class1.jpg"
+            }]
+          }],
+          starredArticles: [{
+              collection: "CSSA简介 TritonTalk 干货攻略 联系我们 CSSA原创 活动预告 往期活动 新生指南",
+              content: ["cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class1.jpg"],
+              title: "课程信息1",
+              summary: "课程简介",
+              thumbnail: "cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class1.jpg"
+            },
+            {
+              collection: "CSSA简介 TritonTalk 干货攻略 联系我们 CSSA原创 活动预告 往期活动 新生指南",
+              content: ["cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class2.jpg"],
+              title: "课程信息2",
+              summary: "课程简介",
+              thumbnail: "cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class2.jpg"
+            }
+          ]
+        })
+        wx.hideLoading()
+      }
+    })
   },
 
   /**
@@ -137,14 +270,97 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    if (!this.data.service) {
+      if (this.data.isSearching) {
+        this.data.searchResult = []
+        wx.showLoading({
+          title: '加载中',
+          mask: true
+        })
+        wx.cloud.callFunction({
+          name: "searchArticles",
+          data: {
+            keyword: this.data.keyword,
+            start: this.data.searchResult.length
+          }
+        }).then((res) => {
+          this.setData({
+            searchResult: this.data.searchResult.concat(res.result.data)
+          })
+          wx.hideLoading()
+          wx.stopPullDownRefresh()
+        })
+      } else {
+        this.data.headlines = []
+        this.data.starredArticles = []
+        this.loadHeadlines()
+        this.loadStarredArticles()
+      }
+    } else {
+      wx.showLoading({
+        title: '加载中',
+        mask: true
+      })
+      this.setData({
+        headlines: [{
+          poster: "cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class1.jpg",
+          articleInfo: [{
+            collection: "CSSA简介 TritonTalk 干货攻略 联系我们 CSSA原创 活动预告 往期活动 新生指南",
+            content: ["cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class1.jpg"],
+            title: "课程信息1",
+            summary: "课程简介",
+            thumbnail: "cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class1.jpg"
+          }]
+        }],
+        starredArticles: [{
+            collection: "CSSA简介 TritonTalk 干货攻略 联系我们 CSSA原创 活动预告 往期活动 新生指南",
+            content: ["cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class1.jpg"],
+            title: "课程信息1",
+            summary: "课程简介",
+            thumbnail: "cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class1.jpg"
+          },
+          {
+            collection: "CSSA简介 TritonTalk 干货攻略 联系我们 CSSA原创 活动预告 往期活动 新生指南",
+            content: ["cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class2.jpg"],
+            title: "课程信息2",
+            summary: "课程简介",
+            thumbnail: "cloud://ucsdcssa-5gxqhwwc12d1b1bf.7563-ucsdcssa-5gxqhwwc12d1b1bf-1305742996/图片/主页/class2.jpg"
+          }
+        ]
+      })
+      wx.hideLoading()
+      wx.stopPullDownRefresh()
+    }
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
+    if (!this.data.service) {
+      if (this.data.isSearching) {
+        wx.cloud.callFunction({
+          name: "searchArticles",
+          data: {
+            keyword: this.data.keyword,
+            start: this.data.searchResult.length
+          }
+        }).then((res) => {
+          this.setData({
+            searchResult: this.data.searchResult.concat(res.result.data)
+          })
+        })
+      }
+    } else {
+      this.setData({
+        searchResult: []
+      })
+    }
+    wx.hideLoading()
   },
 
   /**
